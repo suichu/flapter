d09a605bd8b5373135e771ea6f36f29803b0be0b
外山拓 <tak@suichu.net>
2020-03-03 00:11:22 +0900

    stack

---
+++ stack.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default styled.div`
+  position: relative;
+`;
+
+export const Positioned = styled.div`
+  position: absolute;
+`;
